adc354d629deb7fdd6009acfbd15e3f2a9d59f01
momensale7 <momensaleh2468@gmail.com>
2025-05-11 03:06:09 +0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentModule } from './modules/payment/payment.module';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './modules/Auth/auth.module';
 
 @Module({
   imports: [ ConfigModule.forRoot({
